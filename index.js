41f33a2cccf83b019f6899aaa32e3f42c7932d89
HossamDaCyborg <hossamhamdy1482002@gmail.com>
2023-04-26 13:39:15 +0200

    mbdtf

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 /** @format */
 
+let wait = (delay) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay * 1000);
+  });
 // Import puppeteer
 import puppeteer from "puppeteer";
 import { config } from "dotenv";
@@ -7,13 +13,25 @@ config();
 
 (async () => {
   // Launch the browser
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
 
   // Create a page
   const page = await browser.newPage();
 
   // Go to your site
-  await page.goto("https://www.google.com/search?q=whats+my+ip");
+  await page.goto("https://accounts.google.com/");
+
+  // await page.evaluate(() => {
+  //   window.scrollBy(0, 200);
+  // });
+
+  await page.type('input[type="email"]', "hossamhamdy01001000@gmail.com");
+  await page.type('[name="session_password"]', process.env.PASSWORD);
+  await page.click('[data-litms-control-urn="login-submit"]');
+
+  await page.waitForNavigation();
+
+  // await wait(25);
 
   // Query for an element handle.
   //   const element = await page.waitForSelector('div > .class-name');
